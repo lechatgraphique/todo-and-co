dd72912a1ad2ed7e86e21557fd9b8259a02557a9
Bastien Clement <49923273+lechatgraphique@users.noreply.github.com>
2020-10-15 17:03:39 +0200

    Script Anonymous User (#4)
    Co-authored-by: Bastien CLEMENT <bclement@assurone-group.com>

---
+++ migrations/Version20201015142631.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201015142631 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $anonymousId = 27;
+
+        $this->addSql('UPDATE task SET user_id ='. $anonymousId. ' WHERE user_id is null');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
